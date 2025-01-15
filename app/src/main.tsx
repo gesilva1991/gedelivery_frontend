dcb6a0caa759649f0270af559fb5ff4a28f8ffaa
gesilva1991 <ge_silva1991@gmail.com>
2025-01-14 21:42:16 -0300

    remoção de Router e adição de BrowserRouter no main.tsx

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -3,18 +3,18 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import Home from "./pages/Home.tsx";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Routes, Route, BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./componets/autenticacao/AuthContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AuthProvider>
-      <Router>
+    <BrowserRouter basename="/gedelivery_frontend">
         <Routes>
           <Route path="/" element={<App />} />
           <Route path="/home" element={<Home />} />
         </Routes>
-      </Router>
+      </BrowserRouter>
     </AuthProvider>
   </StrictMode>
 );
