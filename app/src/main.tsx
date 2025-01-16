0229c9ecbf8a4c118fa88fff2c3e6ec9118b8d42
gesilva1991 <ge_silva1991@gmail.com>
2025-01-16 13:08:47 -0300

    fix

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,20 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
-import Home from "./pages/Home.tsx";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./componets/autenticacao/AuthContext.tsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AuthProvider>
-    <BrowserRouter basename="/">
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/home" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
-  </StrictMode>
-);
+    <App />
+  </StrictMode>,
+)
